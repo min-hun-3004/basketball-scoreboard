cdfc98ea8bdac9334843fcaa71cc9158eb03ebb0
Minh Hung Pham <wk.minhhungs@gmail.com>
2024-05-08 11:49:42 +0200

    Web Functional Updates
    Add new btn "New Round" with functions:
    * Update score of each round
    * After 4 rounds -> sum up all the scores
    
    "New game" btn -> reset everything back to initial state
    
    However, JS code wasn't clean -> still need renovation and improvment

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 let homeScore = 0;
 let guestScore = 0;
+let finalHomeScore = 0;
+let finalGuestScore = 0;
 
-let currentRoundCount = 1;
+let currentRoundCounter = 0;
+
+let roundOneScores = document.querySelector("#round-1");
+let roundTwoScores = document.querySelector("#round-2");
+let roundThreeScores = document.querySelector("#round-3");
+let roundFourScores = document.querySelector("#round-4");
+
+let finalScore = document.querySelector("#final-score");
 
 function plusOnePointHome() {
     homeScore += 1;
@@ -34,13 +43,52 @@ function plusThreePointsGuest() {
 }
 
 function newGame() {
-    console.log(homeScore);
-    console.log(guestScore);
-    currentRoundCount += 1;
-
     homeScore = 0;
     guestScore = 0;
     document.getElementById("home-score").textContent = homeScore;
     document.getElementById("guest-score").textContent = guestScore;
+
+    roundOneScores.textContent = homeScore + " - " + guestScore;
+    roundTwoScores.textContent = guestScore + " - " + guestScore;
+    roundThreeScores.textContent = guestScore + " - " + guestScore;
+    roundFourScores.textContent = guestScore + " - " + guestScore;
+    finalScore.textContent = homeScore + " - " + guestScore;
+}
+
+function newRound() {
+    currentRoundCounter += 1;
+    console.log(currentRoundCounter);
+    if (currentRoundCounter === 1) {
+        roundOneScores.textContent = homeScore + " - " + guestScore;
+        finalHomeScore += homeScore;
+        finalGuestScore += guestScore;
+        homeScore = 0;
+        guestScore = 0;
+        document.getElementById("home-score").textContent = homeScore;
+        document.getElementById("guest-score").textContent = guestScore;
+    } else if (currentRoundCounter === 2) {
+        roundTwoScores.textContent = homeScore + " - " + guestScore;
+        finalHomeScore += homeScore;
+        finalGuestScore += guestScore;
+        homeScore = 0;
+        guestScore = 0;
+        document.getElementById("home-score").textContent = homeScore;
+        document.getElementById("guest-score").textContent = guestScore;
+    } else if (currentRoundCounter === 3) {
+        roundThreeScores.textContent = homeScore + " - " + guestScore;
+        finalHomeScore += homeScore;
+        finalGuestScore += guestScore;
+        homeScore = 0;
+        guestScore = 0;
+        document.getElementById("home-score").textContent = homeScore;
+        document.getElementById("guest-score").textContent = guestScore;
+    } else if (currentRoundCounter === 4) {
+        roundFourScores.textContent = homeScore + " - " + guestScore;
+        finalHomeScore += homeScore;
+        finalGuestScore += guestScore;
+        homeScore = 0;
+        guestScore = 0;
+        finalScore.textContent = finalHomeScore + " - " + finalGuestScore;
+    }
 
 }
